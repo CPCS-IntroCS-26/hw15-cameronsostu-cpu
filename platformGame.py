8a8a40197544c1d5133400d3a248c722da0ad16e
cameronsostu-cpu <cameron.so.stu@cedarpark.org>
2026-05-20 09:43:33 -0700

    finished game 5/20

--- platformGame.py
+++ platformGame.py
@@ -17,21 +17,30 @@ platforms = [
     Rect((650, 220), (100, 20)),
     Rect((0, 250), (120, 20)),
     Rect((400, 100), (120, 20)),
-    Rect((100, 100), (120, 20)),
+   Rect((150, 150), (120, 20)),
     Rect((0,50), (120, 20)),
     # bottom floor
     Rect((300, 820), (300, 20) ),
     Rect((700, 850), (80, 20)),
     Rect((450, 740), (50, 20)),
-    Rect((430, 640), (90, 20))
-
+    Rect((430, 640), (90, 20)),
+    Rect((50, 750,), (100,20)),
+    Rect((200, 620),(150,20))
 ]
 
 # Collectibles
 coins = [
     Rect((250, 340), (20, 20)),
     Rect((500, 260), (20, 20)),
-    Rect((690, 180), (20, 20))]
+    Rect((690, 180), (20, 20)),
+    Rect((465, 720), (20, 20)),
+    Rect((100, 850), (20, 20)),
+    Rect((20, 180), (20, 20)),
+    Rect((50, 20), (20, 20)),
+    Rect((450, 20), (20, 20)),
+    Rect((275, 570), (20,20))
+    ]
+    
 score = 0
 lives = 3
 
@@ -41,10 +50,15 @@ lavas =  [
     Rect((350, 450), (100, 20)),
     Rect((600,820), (20, 150)),
     Rect((450,450), (50, 200)),
-    Rect((450, 750), (50,70))
+    Rect((450, 750), (50,70)),
+    Rect((100,250), (20,100)),
+    Rect((0,800),(200, 20)),
+    Rect((200,800), (20,130))
+
+
 ]
 
-goal = Rect((0, 920), (40, 50 ))
+goal = Rect((530, 900), (40, 50 ))
 game_won = False
 game_over = False
 
@@ -63,9 +77,11 @@ def draw():
     for lava in lavas:
         screen.draw.filled_rect(lava, "red")
     screen.draw.filled_rect(goal, "indigo")
-    if game_won:
+    if game_won and score > 8:
+        screen.clear()
         screen.draw.text("You Win!", center=(400, 250), fontsize=60, color="yellow")
     if game_over:
+        screen.clear()
         screen.draw.text("Game Over!", center=(400, 250), fontsize=60, color="red")
 def update():
     global velocity_y, on_ground, score, game_won, lives, game_over
